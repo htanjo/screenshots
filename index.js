9b590bf73078ce1f30c9eb38909802b2cb613395
Hiroyuki Tanjo <expheno@gmail.com>
2016-05-26 17:11:27 +0900

    Create project

---
+++ index.js
@@ -0,0 +1,26 @@
+var Pageres = require('pageres');
+var Promise = require('bluebird');
+var urls = require('./urls.json');
+
+function start() {
+  console.log('Starting...');
+  Promise.map(urls, capture, {concurrency: 5})
+    .then(function () {
+      console.log('Complete!');
+    })
+    .catch(function (err) {
+      console.error('Error!');
+    });
+}
+
+function capture(url) {
+  return new Pageres({delay: 2})
+    .src(url, ['1024x768'])
+    .dest('screenshots')
+    .run()
+    .then(function () {
+      console.log('  captured: ' + url);
+    });
+}
+
+start();
